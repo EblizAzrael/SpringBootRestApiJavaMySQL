85794bfdf266c3eb0773e690c56d5c24fa8d5caf
ebliz <angel.ivan.guadarrama@gmail.com>
2021-12-17 13:58:03 -0600

    commit inicial

---
+++ src/main/java/com/aguadarramal/apirest/SpringBootRestApiJavaMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.aguadarramal.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiJavaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiJavaMySqlApplication.class, args);
+	}
+
+}
